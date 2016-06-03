6a28793a65160ba135629f22dd2d51da5148a338
Reilly Watson <reillywatson@gmail.com>
2016-06-03 18:01:57 -0400

    gofmt

--- app.go
+++ app.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"html/template"
 	//for extracting service credentials from VCAP_SERVICES
 	//"github.com/cloudfoundry-community/go-cfenv"
 )
@@ -14,12 +14,12 @@ const (
 )
 
 var index = template.Must(template.ParseFiles(
-  "templates/_base.html",
-  "templates/index.html",
+	"templates/_base.html",
+	"templates/index.html",
 ))
 
 func helloworld(w http.ResponseWriter, req *http.Request) {
-  index.Execute(w, nil)
+	index.Execute(w, nil)
 }
 
 func main() {
@@ -30,7 +30,7 @@ func main() {
 
 	http.HandleFunc("/", helloworld)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
+
 	log.Printf("Starting app on port %+v\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
