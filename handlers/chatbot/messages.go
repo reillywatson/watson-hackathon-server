99f5ccbb5c776d17f4ce9ed2133e4d462a1823a1
Reilly Watson <reillywatson@gmail.com>
2016-06-05 00:03:21 -0400

    return class, for testing, add a few more classes

--- handlers/chatbot/messages.go
+++ handlers/chatbot/messages.go
@@ -2,6 +2,7 @@ package chatbot
 
 import (
 	"bytes"
+	"fmt"
 	"math/rand"
 	"text/template"
 )
@@ -9,23 +10,29 @@ import (
 var messagesByClass = map[string][]string{
 	"confused":           {"I don't understand!", "I'm confused!", "I don't get it"},
 	"Current heart rate": {"Your current heart rate is {{.current_pulse}} BPM"},
+	"Bad Calories":       {"Don't eat it! you dummy"},
+	"Daily Calories": {
+		"Some calorie sources are better than others. Calories from sugar without fiber are empty calories. While 250 calories from a candy bar are utilized the same as the combined 250 calories from a banana, an apple, and a pear, the fruit is obviously much better for you.",
+		"The number of calories you need depends on your age, body size, and activity levels -- most teens and adults need somewhere around 1,500 to 2,500 calories per day.",
+	},
+	"Pushups":        {"https://www.youtube.com/watch?v=Eh00_rniF8E"},
+	"Situps":         {"https://www.youtube.com/watch?v=1fbU_MkV7NE"},
+	"Max heart rate": {"Your max heart rate is {{.max_pulse}}"},
 }
 
 /*
 "Bad Calories":	{"Don't eat it! you dummy"}
 Calories	"""<if {{.gender}} == """"female"""" then <655 + (4.35 x {{.weight_in_pounds}}) + (4.7 x {{.height_in _inches}}) - (4.7 x {{.age}}) else <66 + (6.23 x {{.weight_in_pounds}}) + (12.7 x {{.height_in _inches}}) - (6.8 x {{.age}})"""
 //Calories	"<if {{.gender}} == ""female"" then <655 + (4.35 x {{.weight_in_pounds}}) + (4.7 x {{.height_in _inches}}) - (4.7 x {{.age}}) else <66 + (6.23 x {{.weight_in_pounds}}) + (12.7 x {{.height_in _inches}}) - (6.8 x {{.age}})"
-Daily Calories	Some calorie sources are better than others. Calories from sugar without fiber are empty calories. While 250 calories from a candy bar are utilized the same as the combined 250 calories from a banana, an apple, and a pear, the fruit is obviously much better for you.
-Daily Calories	The number of calories you need depends on your age, body size, and activity levels -- most teens and adults need somewhere around 1,500 to 2,500 calories per day.
 Duration	You have been doing it for {{.duration}}
 Exercise Time	You'll need one minute. Ready?
 Improvement	You doing better and better, keep it up!
 Jumping Jack	https://www.youtube.com/watch?v=p64YlMRIDVM
-Max heart rate	Your max heart rate is {{.max_pulse}}
+Max heart rate
 Overall heart rate	You recomended maximum heart rate is <220 - {{.age}}>
 Overall heart rate	Your target heart rate is {{.max_pulse}}
-Pushups	https://www.youtube.com/watch?v=Eh00_rniF8E
-Situp	https://www.youtube.com/watch?v=1fbU_MkV7NE
+Pushups
+Situp
 Target heart rate	Your target heart rate is  {{.max_pulse}}
 Time	Time is not important, the fact that you doing it is. Keep it up
 */
